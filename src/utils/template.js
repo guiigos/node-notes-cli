2a4f477897198ca91763f4b8ffb09283b97c688e
Guilherme Alves <guiigos.alves@gmail.com>
2020-07-18 07:38:38 -0300

    Change print note

--- src/utils/template.js
+++ src/utils/template.js
@@ -4,7 +4,8 @@ const template = (notes) => {
   if (notes.length === 0) return;
 
   console.log(colors.bold.yellow('Notes'));
-  notes.forEach(({ id, text }) => console.log(`- ${colors.bold(id)} ${colors.italic(text)}\n`));
+  notes.forEach(({ id, text }) => console.log(`- ${colors.bold(id)} ${colors.italic(text)}`));
+  console.log('\n');
 }
 
 module.exports = template;
